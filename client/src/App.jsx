be63973a8d4784d98bb270181d08da4a18c0bf4d
VivekSreenivasan <vivek.sreenivasan3@gmail.com>
2020-11-17 18:06:25 -0500

    Added route to handle showing place bet page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import { selectUser } from './store/slices/userSlice';
 import Dashboard from './dashboard/Dashboard';
 import Home from './home/Home';
 import './App.css';
+import BetPage from './dashboard/BetPage';
 
 function App() {
   const user = useSelector(selectUser);
@@ -24,6 +25,7 @@ function App() {
           <Route exact path="/dashboard">
             {!user.name ? <Redirect to="/" /> : <Dashboard /> }
           </Route>
+          <Route path="/betpage/:id" component={BetPage} />
         </Switch>
       </Router>
     </div>
