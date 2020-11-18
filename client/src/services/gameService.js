3cdd27176ab7f67e03425bcc49111f950e42e3d1
VivekSreenivasan <vivek.sreenivasan3@gmail.com>
2020-11-18 14:47:05 -0500

    Added api to get the weekly API scores

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -10,5 +10,9 @@ export default {
     const res = await axios.get(`/api/teams/${id}`);
     return res.data[0].name || [];
   },
+  updateScores: async () => {
+    const res = await axios.get('/api/fetch_weekly_scores');
+    return res.data || [];
+  },
 
 };
