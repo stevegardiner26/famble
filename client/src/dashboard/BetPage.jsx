914e64ad7afd90855056ce1f6d8187bc3b9636f4
VivekSreenivasan <vivek.sreenivasan3@gmail.com>
2020-11-18 16:33:19 -0500

    Added the useffect function

--- client/src/dashboard/BetPage.jsx
+++ client/src/dashboard/BetPage.jsx
@@ -1,4 +1,15 @@
-import React from 'react';
+/* eslint-disable no-undef */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-alert */
+import React, { useEffect, useState } from 'react';
+import {
+  Col, Button, Form, FormGroup, Label, Input,
+} from 'reactstrap';
+import { useSelector } from 'react-redux';
+import styles from './BetModal.module.css';
+import betService from '../services/betService';
+import { selectUser } from '../store/slices/userSlice';
 
 export default function BetPage(props) {
   // eslint-disable-next-line react/destructuring-assignment
@@ -9,6 +20,22 @@ export default function BetPage(props) {
   const { homeTeamID } = props.location.game;
   // eslint-disable-next-line react/destructuring-assignment
   const { awayTeamID } = props.location.game;
+  useEffect(() => {
+    if (valid) {
+      const setBet = async () => {
+        const res = await betService.createBet(userID, gameID, teamID, amount);
+        if (res === []) {
+          alert('Could not place bet at this time. Try again later.');
+        } else {
+          alert('Bet placed successfully!');
+          setValid(false);
+          setModal(!modal);
+        }
+      };
+      setBet();
+    }
+  }, [valid, userID, gameID, teamID, amount]);
+
   return (
     <div>
       <h3>{homeTeam}</h3>
