e0ebcc4159eb98777be95ccddaafa2c5acaa1be5
Pedro Ramos <pramos1996@Outlook.com>
2020-11-24 10:26:05 -0500

    Made small changed to routes

--- routes/statisticRoutes.js
+++ routes/statisticRoutes.js
@@ -1,11 +1,9 @@
 const { 
-    fetchTeamStats,
     getStatsByTeamId
   } = require('./statisticRoutesHandlers');
   
   module.exports = (app) => {
     // Get
-    app.get('/api/stats/teams/fetch_stats', fetchTeamStats);
     app.get('/api/bets/teams/:team_id', getStatsbyTeamId);
     
   };
