e0e459dfbe7aedc7d07f08ee065778a64aba1dc9
Pedro Ramos <pramos1996@Outlook.com>
2020-11-24 10:24:40 -0500

    removed a function

--- routes/statisticRoutesHandlers.js
+++ routes/statisticRoutesHandlers.js
@@ -20,33 +20,6 @@ async function getStatsByTeamID(req, res){
     });
 }
 
-// app.get('/api/stats/teams/fetch_stats', fetchStats)
-async function fetchTeamStats(req, res){
-    client.get("https://api.sportsdata.io/v3/nfl/scores/json/UpcomingSeason", {headers: {"Ocp-Apim-Subscription-Key": process.env['NFL_API_TOKEN']}}, function (year, response) {
-        client.get(`https://api.sportsdata.io/v3/nfl/scores/json/TeamSeasonStats/${year}`, {headers: {"Ocp-Apim-Subscription-Key": process.env['NFL_API_TOKEN']}}, function (data, response) {
-            data.forEach(async (stat) => {
-                let current_team = await Team.findOne({team_id: stat.Team});
-                if(!current_team){
-                    let statistics = {
-                        team_id: stat.Team,
-                        touchdowns: stat.Touchdowns,
-                        passing_attempts: stat.PassingAttempts,
-                        completion_percentage: stat.CompletionPercentage,
-                        passing_yards: stat.PassingYards,
-                        fumbles_forced: stat.FumblesForced,
-                        rushing_yards: stat.RushingYards,
-                        penalty_yards: stat.PenaltyYards,
-                        sacks: stat.Sacks,
-                    };
-                    await Stat.create(statistics);
-                }
-            });
-            return res.status(201).send({
-                message: "Imported All Team Stats!",
-            });
-        });
-    });
-}
 exports.client = client;
 exports.fetchTeamStats = fetchTeamStats;
 exports.getStatsByTeamID = getStatsByTeamID;
