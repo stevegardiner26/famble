24dd2a11dcf8373e8422db9879dfa3e73b1fe2fe
ranajay5699 <ranajay5699@gmail.com>
2020-12-03 18:38:27 -0500

    adjust url

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -11,7 +11,7 @@ export default {
     return res.data || [];
   },
   getUsers: async () => {
-    const res = await axios.get('/api/users/');
+    const res = await axios.get('/api/users');
     return res.data || [];
   },
 };
