94b7a199542345539c493c547779b336066fddf5
ranajay5699 <ranajay5699@gmail.com>
2020-12-06 12:54:04 -0500

    updated user test

--- test/userTest.js
+++ test/userTest.js
@@ -2,7 +2,7 @@ const httpMocks = require('node-mocks-http');
 const sinon = require('sinon');
 const assert = require('chai').assert;
 const { userModel } = require('../models/User');
-const { getUserById, createUser } = require('../routes/userRoutesHandlers');
+const { getUserById, createUser, getUsers } = require('../routes/userRoutesHandlers');
 
 describe("GET /api/users/:id", function() {  
   let mockFindById;
@@ -117,7 +117,7 @@ describe("POST /api/users", function() {
   });
 });
 
-describe("POST /api/users", function() {  
+describe("POST /api/users 1", function() {  
   let mockFindOne;
   const fakeUser = {
     shreddit_balance: 7770,
@@ -168,5 +168,63 @@ describe("POST /api/users", function() {
     });
 
     createUser(mockRequest, mockResponse);
+  });
+});
+
+describe("GET /api/users/rank/:id", function() {  
+  let mockFind;
+  let fakeUsers = [{
+    name: "Jay Rana",
+    profile_image: "https://lh3.googleusercontent.com/a-/AOh14Gg629IeUkf1lWpBcSqr7-m_pEhpvQI3CdAczWijeA=s96-c",
+    email: "jpr48@njit.edu",
+    google_id: "108376284041323611441",
+    id: "5fb83983417ae836a4e2b1701234"
+  }, 
+  {
+    name: "Jay Rana",
+    profile_image: "https://lh3.googleusercontent.com/a-/AOh14Gg629IeUkf1lWpBcSqr7-m_pEhpvQI3CdAczWijeA=s96-c",
+    email: "jpr48@njit.edu",
+    google_id: "108376284041323611441",
+    id: "5fb83983417ae836a4e2b170"
+  }]
+
+  beforeEach((done) =>{
+    mockFind = sinon.stub(userModel, "find").returns({sort: sinon.stub().returns(fakeUsers)});
+    done();
+  })
+
+  afterEach((done) => {
+    mockFind.restore();
+    done();
+  })
+
+  it("it should have status code 200 and rank of user and all users", function(done) {
+    const mockRequest = httpMocks.createRequest({
+      method: "GET",
+      url: "/api/users/rank/5fb83983417ae836a4e2b170",
+      params:{
+        id: "5fb83983417ae836a4e2b170"
+      }
+    });
+    const mockResponse = httpMocks.createResponse({
+      eventEmitter: require('events').EventEmitter
+    });
+    
+    mockResponse.on('end', function() {
+      const expected = {
+        users: fakeUsers,
+        rank: 2
+      };
+      try {
+        assert.strictEqual(mockResponse.statusCode, 200);
+        assert.deepStrictEqual(mockResponse._getData(), expected);
+        done();
+      }
+      catch (error){
+        done(error);
+      }
+    });
+
+    getUsers(mockRequest, mockResponse);
   });
 });
